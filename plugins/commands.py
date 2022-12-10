55c85359b646c3bbd45cfb4d4e1e5f1bdafdf89c
Dsdstam <120117100+Dsdstam@users.noreply.github.com>
2022-12-10 18:12:34 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://t.me/MM_MOVIESSS') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://t.me/Movies_Tamizhaa') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
